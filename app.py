50f5328140146de951cf1be59fe18eef29fa538f
Marco Barozzi <mbarozzi@gmail.com>
2023-02-16 00:11:07 +0000

    incluindo dados no MOCK

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, url_for, session,flash,g
 
 app = Flask(__name__)
 
+# KEY
+SECRET_KEY = 'max'
+app.config.from_object(__name__)
 # POST MOCK
 
 posts = [
@@ -26,7 +29,46 @@ posts = [
 
 ]
 
+# USER MOCKS
 
-@app.route("/")
-def helloPovu():
-    return render_template("exibir_entradas.html",entradas=posts)
+USERNAME = "admin"
+PASSWORD = "12345"
+
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    return render_template("exibir_entradas.html", entradas=posts)
+
+
+@app.route("/quaqua", methods=["GET", "POST"])
+def login():
+    erro = ""
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            return redirect(url_for('main'))
+
+        erro = "usuario ou senha invalidos"
+    return render_template("login.html", erro=erro)
+
+
+@app.route("/inserir",methods=["POST"])
+def inserir_entrada():
+    
+    novo_post = {
+        
+        "titulo": request.form['titulo'],
+        "texto": request.form['texto'],
+         
+    }
+ 
+    posts.append(novo_post)
+    #return posts
+    return redirect(url_for('main'))
+
+
+@app.route("/logout")
+def logout():
+    session.pop('logado', None)
+    flash('Logout feito')
+    return redirect(url_for('main'))
